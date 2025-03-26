9361719ef0231ef4c748b85646f243dc3f04ed68
sabrina0809 <souzalaurindo49@gmail.com>
2025-03-26 20:36:25 -0300

    fazendo calculadora 2.0

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function somar() {
+    let numero1 = parseFloat (document.getElementById('numero1').value);
+    let numero2 = parseFloat (document.getElementById('numero2').value);
+    
+    let resultado = numero1 + numero2;
+
+    document.getElementById('resultado').innerText = resultado
+}
+
+
+function dividir() {
+    let numero1 = parseFloat (document.getElementById('numero1').value);
+    let numero2 = parseFloat (document.getElementById('numero2').value);
+
+    
+    if(numero2 === 0){
+        document.getElementById('resultado').innerText = "divisão por zero.";
+    }else{
+        let resultado = numero1 / numero2;
+        document.getElementById('resultado').innerText = resultado
+    }
+}
+
